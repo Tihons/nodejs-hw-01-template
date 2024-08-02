9fcc289af6eaf9c9c1c9a98dc1ead7f098932c6f
Tihons <tihonshramenko@gmail.com>
2024-08-02 14:40:41 +0200

    save

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,5 @@
-export const PATH_DB =
+import path from 'path';
+
+const pathWithDb = path.join(process.cwd());
+
+export const PATH_DB = path.join(pathWithDb, 'src', 'db', 'db.json');
--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const countContacts = async () => {};
+export const countContacts = async () => {
+  try {
+    return JSON.parse(await fs.readFile(PATH_DB, 'utf-8')).length;
+  } catch (error) {
+    console.log(error, 'Failed to read file');
+  }
+};
 
-console.log(await countContacts());
+console.log(`Count contacts to db: ${await countContacts()}`);
